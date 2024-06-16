17869b8d6a6eaf0cba72d7835f913e23bc7d8458
AtharvShelke <shelkeatharv964@gmail.com>
2024-06-16 15:30:18 +0530

    update header.php

--- partials/header.php
+++ partials/header.php
@@ -15,7 +15,7 @@ if(isset($_SESSION['user-id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Multipage Blog Website</title>
+    <title>Responsive Multipage Event Listing Website</title>
 
     <link rel="stylesheet" href="./css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
