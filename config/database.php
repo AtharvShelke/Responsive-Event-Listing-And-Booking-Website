0af55cc5be202217e4184a30d7a618c14693f35b
AtharvShelke <shelkeatharv964@gmail.com>
2024-07-01 18:29:33 +0530

    done

--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 
     require 'constants.php';
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWD, DB_NAME);
-if (mysqli_errno($conn)) {
- die("Connection failed");
-}
+    if (mysqli_errno($conn)) {
+        die("Connection failed");
+    }
 
 ?>
